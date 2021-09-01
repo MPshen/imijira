3f0edf3f7c7248588ecc528b530cce9aff6f76c0
MPshen <shenmengping19@mails.ucas.ac.cn>
2021-09-01 21:23:01 +0800

    useDebounce

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import { cleanObject, useMount, useDebounce } from "../../utils";
+// import * as qs from "qs";
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+    const [param, setParam] = useState({
+        name:'',
+        personId:''
+    })
+    const [users, setUsers] = useState([])
+    const [list, setList] = useState([])
+
+    const debounceParam = useDebounce(param,1000)
+
+    useEffect(()=>{
+        // fetch(`http://localhost:3001/projects?name=${param.name}&personId=${param.personId}`).then(async response => {
+        // fetch(`http://localhost:3001/projects`).then(async response => {
+        // fetch(`http://localhost:3001/projects?${qs.stringfy(cleanObject(param))}`).then(async response => {
+        // fetch(`http://localhost:3001/projects?name=${cleanObject(param).name}&personId=${cleanObject(param).personId}`).then(async response => {
+        fetch(`http://localhost:3001/projects?${debounceParam.name?`name=${debounceParam.name}&`:''}${debounceParam.personId?`personId=${debounceParam.personId}`:''}`).then(async response => {
+            if(response.ok){
+                setList(await response.json())
+            }
+        })
+    },[debounceParam])
+
+    useMount(()=>{
+        fetch(`http://localhost:3001/users`).then(async response => {
+            if(response.ok){
+                setUsers(await response.json())
+            }
+        })
+    })
+
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam}/>
+        <List users={users} list={list}/>
+    </div>
+}
---
+++ src/utils/index.js
@@ -0,0 +1,30 @@
+import {useEffect,useState} from "react";
+export const isFalse = (value) => value === 0?false:!value
+export const cleanObject = (object) => {
+    const result = {...object}
+    Object.keys(result).forEach(key =>{
+        const value = result[key]
+        if(isFalse(value)){
+            delete result[key]
+        }
+    })
+    return result
+}
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback();
+        
+    }, [])
+}
+
+export const useDebounce = (value,delay) => {
+    const [debounceValue, setdebounceValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout( () => setdebounceValue(value),delay )
+        return () => clearTimeout(timeout)        
+    }, [value,delay])
+
+    return debounceValue
+}
