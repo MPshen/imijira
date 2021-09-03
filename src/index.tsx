732c0d55b614bf1f52576b63107c3907d8de0e7a
MPshen <shenmengping19@mails.ucas.ac.cn>
2021-09-04 00:11:46 +0800

    使用useContext存储全局用户信息

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {loadDevTools} from 'jira-dev-tool'
+import {loadDevTools} from 'jira-dev-tool';
+import {AppProviders} from "./context";
 
-loadDevTools(() => ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+loadDevTools(() => 
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
 )
 
 
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,23 +1,24 @@
 import React, { FormEvent } from "react";
+import {useAuth} from "../../context/auth-context";
 const apiUrl = process.env.REACT_APP_API_URL
 
 
 export const LoginScreen = () => {
-    const login = (param:{ username:string, password:string}) => {
-        fetch(`${apiUrl}/login`,{
-        // fetch(`http://localhost:3001/login`,{
-            method: 'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(param)
-        }).then(
-            async (response: Response) => {
-                if(response.ok){
-                }
-            }
-        );
-    }
+    const {login, user} = useAuth();
+    // const login = (param:{ username:string, password:string}) => {
+    //     fetch(`${apiUrl}/register`,{
+    //         method: 'POST',
+    //         headers:{
+    //             'Content-Type':'application/json'
+    //         },
+    //         body: JSON.stringify(param)
+    //     }).then(
+    //         async (response: Response) => {
+    //             if(response.ok){
+    //             }
+    //         }
+    //     );
+    //}
 
 
     // HTMLFormElement extends Element
@@ -27,15 +28,24 @@ export const LoginScreen = () => {
         const password:string = (event.currentTarget[1] as HTMLFormElement).value
         login({username,password})
     }
-    return <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="username">用户名</label>
-            <input type="text" id={'username'}/>
-        </div>
-        <div>
-            <label htmlFor="password">密码</label>
-            <input type="password" id={'password'}/>
-        </div>
-        <button type={"submit"}>登录</button>
-    </form>
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {
+                user ? <div>
+                    登录成功，用户名：{user?.name}
+                    token:{user.token}
+                </div> : null
+            }
+            <div>
+                <label htmlFor="username">用户名</label>
+                <input type="text" id={'username'}/>
+            </div>
+            <div>
+                <label htmlFor="password">密码</label>
+                <input type="password" id={'password'}/>
+            </div>
+            <button type={"submit"}>登录</button>
+        </form>
+    )
 }
