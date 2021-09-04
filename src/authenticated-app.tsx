060cbffa0328be4cfe18302e44692eab33029a32
MPshen <shenmengping19@mails.ucas.ac.cn>
2021-09-05 02:22:10 +0800

    style:用row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { Row } from "./components/lib";
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
 
@@ -7,8 +8,8 @@ export const AuthenticatedApp = () => {
     const {logout} = useAuth()
     return (
         <Container>
-            <Header>
-                <HeaderLeft>
+            <Header between={true}>
+                <HeaderLeft gap={true}>
                     <h3>Logo</h3>
                     <h3>项目</h3>
                     <h3>用户</h3>
@@ -17,12 +18,9 @@ export const AuthenticatedApp = () => {
                     <button onClick={logout}>登出</button> 
                 </HeaderRight>                               
             </Header>
-            <Nav>nav</Nav>
             <Main>
                 <ProjectListScreen/>                
             </Main>
-            <Aside>asside</Aside>
-            <Footer>footer</Footer>
         </Container>
     ); 
 }
@@ -30,29 +28,12 @@ export const AuthenticatedApp = () => {
 const Container = styled.div`
 display: grid;
 grid-template-rows: 6rem 1fr 6rem;
-grid-template-columns: 20rem 1fr 20rem;
-grid-template-areas: 
-    "header header header"
-    "nav main aside"
-    "footer footer footer"
-;
+/* grid-template-columns: 20rem 1fr 20rem; */
 height: 100vh;
 `
 
 // grid-area 用来给grid子元素起名字
-const Header = styled.header`
-grid-area: header;
-display:flex;
-flex-direction:row;
-align-items:center;
-justify-content:space-between;
-`
-const HeaderLeft = styled.div`
-display: flex;
-align-items: center;
-`
+const Header = styled(Row)``
+const HeaderLeft = styled(Row)``
 const HeaderRight = styled.div``
-const Main = styled.main`grid-area:main;`
-const Nav = styled.nav`grid-area:nav;`
-const Aside = styled.aside`grid-area:aside;`
-const Footer = styled.footer`grid-area:footer;`
+const Main = styled.main``
