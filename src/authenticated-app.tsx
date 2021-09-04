93e8003c2a4e40a13523112ba4bcb6361c85d6ab
MPshen <shenmengping19@mails.ucas.ac.cn>
2021-09-05 03:11:07 +0800

    style:优化项目列表页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -3,19 +3,29 @@ import React from "react";
 import { Row } from "./components/lib";
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
+import {ReactComponent as SoftwareLogo} from "./assets/software-logo.svg"
+import { Dropdown, Menu } from "antd";
 
 export const AuthenticatedApp = () => {
-    const {logout} = useAuth()
+    const {logout,user} = useAuth()
     return (
         <Container>
             <Header between={true}>
                 <HeaderLeft gap={true}>
-                    <h3>Logo</h3>
+                    <SoftwareLogo width={'18rem'} color={'rgb(38, 132,255'}/>
                     <h3>项目</h3>
                     <h3>用户</h3>
                 </HeaderLeft>
                 <HeaderRight>
-                    <button onClick={logout}>登出</button> 
+                    <Dropdown overlay={<Menu>
+                        <Menu.Item key={'logout'}>
+                            <a onClick={logout}>登出</a>
+                        </Menu.Item>
+                    </Menu>}>
+                        <a onClick={e => e.preventDefault()}>
+                            Hi,{user?.name}
+                        </a>
+                    </Dropdown>                    
                 </HeaderRight>                               
             </Header>
             <Main>
@@ -33,7 +43,11 @@ height: 100vh;
 `
 
 // grid-area 用来给grid子元素起名字
-const Header = styled(Row)``
+const Header = styled(Row)`
+padding:3.2rem;
+box-shadow: 0 0 5px 0 rgba(0,0,0,0.1);
+z-index:1;
+`
 const HeaderLeft = styled(Row)``
 const HeaderRight = styled.div``
 const Main = styled.main``
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,5 +1,7 @@
-import { Input, Select } from "antd";
-import React from "react";
+/* @jsxImportSource @emotion/react */
+import {jsx} from '@emotion/react'
+import { Form, Input, Select } from "antd";
+import React, { ChangeEvent } from "react";
 // import { useEffect,useState } from "react";
 
 export interface User{
@@ -19,24 +21,33 @@ interface SearchPanelProps{
     },
     setParam:(param:SearchPanelProps['param'])=>void;
 }
+
+
 export const SearchPanel = ({users, param, setParam}:SearchPanelProps) => {
-    
-    console.log(users)
-    return <form>
-        <div>
-            <Input type="text" value={param.name} onChange={evt => setParam({
-                ...param,
-                name: evt.target.value
-            })}/>
-            <Select value={param.personId} onChange={value => setParam({
-                ...param,
-                personId: value
-            })}>
-                <Select.Option value={''}>负责人</Select.Option>              
-                {
-                    users.map(user => <Select.Option key={user.id} value={user.id}>{user.name}</Select.Option>)                    
-                }                
-            </Select>
-        </div>
-    </form>
+    return(
+        <Form css={{marginBottom:'2rem'}} layout={"inline"}>
+            <Form.Item>
+                <Input 
+                    placeholder={'项目名'}
+                    type="text" 
+                    value={param.name} 
+                    onChange={(evt:ChangeEvent<HTMLInputElement>) => 
+                        setParam({
+                            ...param,
+                            name: evt.target.value
+                })}/>
+            </Form.Item>
+            <Form.Item>
+                <Select value={param.personId} onChange={value => setParam({
+                    ...param,
+                    personId: value
+                })}>
+                    <Select.Option value={''}>负责人</Select.Option>              
+                    {
+                        users.map(user => <Select.Option key={user.id} value={user.id}>{user.name}</Select.Option>)                    
+                    }                
+                </Select>
+            </Form.Item>
+        </Form>
+    )
 }
