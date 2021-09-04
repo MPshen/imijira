378e2305edf0fd3c1167cffce85ebfce32fbb6b2
MPshen <shenmengping19@mails.ucas.ac.cn>
2021-09-04 16:44:05 +0800

    维持登录状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,8 +3,10 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import { useMount, useDebounce, cleanObject } from "../../utils";
-import * as qs from "qs";
-const apiUrl = process.env.REACT_APP_API_URL
+// import * as qs from "qs";
+import { useHttp } from "../../utils/http";
+// const apiUrl = process.env.REACT_APP_API_URL
+
 
 export const ProjectListScreen = () => {
     const [param, setParam] = useState({
@@ -13,30 +15,33 @@ export const ProjectListScreen = () => {
     })
     const [users, setUsers] = useState([])
     const [list, setList] = useState([])
+    const client = useHttp()
 
     const debounceParam = useDebounce(param,1000)
 
     useEffect(()=>{
         // fetch(`http://localhost:3001/projects?${debounceParam.name?`name=${debounceParam.name}&`:''}${debounceParam.personId?`personId=${debounceParam.personId}`:''}`).then(async response => {
         // fetch(`http://localhost:3001/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
-            if(response.ok){
-                setList(await response.json())
-            }
-        })
+        client('projects',{data: cleanObject(debounceParam)}).then(setList)
+        // fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
+        //     if(response.ok){
+        //         setList(await response.json())
+        //     }
+        // })
     },[debounceParam])
 
     useMount(()=>{
         // fetch(`http://localhost:3001/users`).then(async response => {
-        fetch(`${apiUrl}/users`).then(async response => {
-            if(response.ok){
-                setUsers(await response.json())
-            }
-        })
-    })
+        client('users').then(setUsers)
+        // fetch(`${apiUrl}/users`).then(async response => {
+        //     if(response.ok){
+        //         setUsers(await response.json())
+        //     }
+        // })
+    });
 
-    return <div>
+    return (<div>
         <SearchPanel users={users} param={param} setParam={setParam}/>
         <List users={users} list={list}/>
-    </div>
+    </div>)
 }
